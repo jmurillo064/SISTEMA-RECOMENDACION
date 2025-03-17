7af048e7f0398db0d33f0f7b4a24e5f10ecf8ae7
jorge.murillo <jorge.murillo@redcode.net.ec>
2025-03-17 00:08:24 -0500

    7 commit

--- models/modelo.py
+++ models/modelo.py
@@ -5,7 +5,7 @@ from sentence_transformers import SentenceTransformer, util
 
 def procesarAbstract(abstract):
     # Cargar modelo de lenguaje
-    nlp = spacy.load("en_core_web_sm")
+    #nlp = spacy.load("en_core_web_sm")
     model = SentenceTransformer("all-MiniLM-L6-v2")
 
     # Taxonomía IEEE (ejemplo reducido)
